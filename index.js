5914907244cdbe11715238c902186735834809f4
olumidtech <olumideadewusis@gmail.com>
2023-06-04 23:02:24 +0100

    Adding API key

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 
 async function axiosGet() {
-
+  const header = {
+    'X-CoinAPI-Key': '91B34A92-F7F6-4D52-BF0D-2E758E1EC4EE'
+  };
 }
 
 async function axiosPost() {
